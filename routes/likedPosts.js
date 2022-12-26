832949bbd9928dc1dced9b2f2a7dc929be06f8ad
kevingarry97 <kevingarry97@gmail.com>
2022-12-26 14:34:58 -0800

    Fixing Vote & Like

--- routes/likedPosts.js
+++ routes/likedPosts.js
@@ -4,8 +4,8 @@ const { LikedPost } = require('../models/likedPost');
 const { Post } = require('../models/post');
 const router = express.Router();
 
-router.get('/liked/post/:id', async (req, res) => {
-    const likedPosts = await LikedPost.find({"post._id": req.params.id});
+router.get('/liked/post', async (req, res) => {
+    const likedPosts = await LikedPost.find();
     if (!likedPosts) return res.status(404).json({message: 'No Liked Posts Found!!'});
 
     res.status(200).send(likedPosts);
@@ -16,7 +16,7 @@ router.post('/like/post', auth, async (req, res) => {
     const post = await Post.findOne({"_id": req.body.post_id})
     if (!post) return res.status(404).send({message: 'No Post Found'});
 
-    let likedPost = await LikedPost.findOne({"post._id": req.body.post_id});
+    let likedPost = await LikedPost.findOne({"post._id": req.body.post_id}) && await LikedPost.findOne({"liker._id": req.user._id});
 
     if(likedPost != undefined) {
         likedPost = await LikedPost.findByIdAndUpdate((likedPost._id) ,{
